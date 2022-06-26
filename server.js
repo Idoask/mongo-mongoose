e4e9d3808d3fd777512af0c81defc69ad1cc56bf
ido200900@gmail.com <ido200900@gmail.com>
2022-06-26 20:08:10 +0300

    first commit

--- server.js
+++ server.js
@@ -2,17 +2,33 @@ const express = require("express");
 
 const app = express();
 
-const products = require("./products.json");
+// mongodb+srv://ido:<password>@cluster0.bpvpxkj.mongodb.net/?retryWrites=true&w=majority
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://admin:VYcgjZKhltEVwKLA@cluster0.bpvpxkj.mongodb.net/test?retryWrites=true&w=majority"
+);
+
+const Product = mongoose.model("Product", {
+  title: String,
+  id: Number,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
+  rating: { rate: Number, count: Number },
+}); //product model
 
 // GET /products - all products
-app.get("/products", function (req, res) {
+app.get("/products", async function (req, res) {
+  const products = await Product.find();
   res.json(products);
 });
 
 // GET /products/:id - return single product by id
-app.get("/products/:id", function (req, res) {
-  const product = products.find((p => p.id === parseInt(req.params.id)));
-  res.json(product)
+app.get("/products/:id", async function (req, res) {
+  const product = await Product.findOne({ id: parseInt(req.params.id) });
+  res.json(product);
 });
 
 app.listen(4000, function () {
